8569bc63428297c55df92d3fbfcb60e2730bb06b
Pasztor Adam <adam.psztr@icloud.com>
2022-10-01 11:46:49 +0200

    add try-catch to functions
    add try-catch to functions of puppeteer and delete unnecessary files

--- config.js
+++ config.js
@@ -2,18 +2,20 @@ module.exports = [
 	{
 		category: "iphone-14-pro",
 		targetPrice: 40,
-		fileName: "fiphone-14-pro.txt",
+		fileName: "iphone-14-pro.txt",
 		products: [
 			{
 				url: "https://www.apple.com/hu/shop/buy-iphone/iphone-14-pro",
 				selectors: {
+					pic: "1",
 					tittle: "span > span.form-selector-left-col.column.large-6 > span.form-selector-title",
 					price: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span"
 				}
 			},
 			{
-				url: "https://istyle.hu/iphone-14-pro.html?config_size=3574&color=3592&config_model=1322",
+				url: "https://pistyle.hu/iphone-14-pro.html?config_size=3574&color=3592&config_model=1322",
 				selectors: {
+					pic: "2",
 					tittle: "#col-info-main > div > div.page-title-wrapper.product > h1 > span",
 					price: "#product-options-wrapper > div > div > div.swatch-attribute.config_model.show_discount_badge.show_price > div > div.swatch-option.selected > span.price"
 				}
@@ -21,38 +23,39 @@ module.exports = [
 			{
 				url: "https://www.mediamarkt.hu/hu/product/_apple-iphone-14-pro-128-gb-asztrofekete-kártyafüggetlen-okostelefon-1396917.html",
 				selectors: {
+					pic: "3",
 					tittle: "#product-details > div.details > h1",
 					price: "#product-details > div.details > div.product-attributes.js-product-attributes > div.product-attributes__group.product-attributes__group--colors > div.product-attributes__color-item.product-attributes__color-item--active > span"
 				}
 			}
 		]
 	},
-	{
-		category: "Fiction",
-		targetPrice: 20,
-		fileName: "fiction_book.txt",
-		products: [
-			{
-				url: "http://books.toscrape.com/catalogue/thirst_946/index.html",
-				selectors: {
-					tittle: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
-					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
-				}
-			},
-			{
-				url: "http://books.toscrape.com/catalogue/tuesday-nights-in-1980_870/index.html",
-				selectors: {
-					tittle: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
-					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
-				}
-			},
-			{
-				url: "http://books.toscrape.com/catalogue/the-first-hostage-jb-collins-2_749/index.html",
-				selectors: {
-					tittle: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
-					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
-				}
-			}
-		]
-	}
+	// {
+	// 	category: "Fiction",
+	// 	targetPrice: 20,
+	// 	fileName: "fiction_book.txt",
+	// 	products: [
+	// 		{
+	// 			url: "http://books.toscrape.com/catalogue/thirst_946/index.html",
+	// 			selectors: {
+	// 				tittle: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
+	// 				price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
+	// 			}
+	// 		},
+	// 		{
+	// 			url: "http://books.toscrape.com/catalogue/tuesday-nights-in-1980_870/index.html",
+	// 			selectors: {
+	// 				tittle: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
+	// 				price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
+	// 			}
+	// 		},
+	// 		{
+	// 			url: "http://books.toscrape.com/catalogue/the-first-hostage-jb-collins-2_749/index.html",
+	// 			selectors: {
+	// 				tittle: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
+	// 				price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
+	// 			}
+	// 		}
+	// 	]
+	// }
 ]
--- index.js
+++ index.js
@@ -29,32 +29,77 @@ const config = require("./config");
 		logger.write(el.category + "\n");
 
 		for (const pruduct of el.products) {
-			count++;
-			console.log(count);
-
+			
 			let date = new Date();
 			date = date.toLocaleDateString("hu", {
 				day: "numeric",
 				month: "short",
 				year: "numeric"
 			});
-
+			
 			//navigate to URL
-			await page.goto(pruduct.url);
-			// await page.screenshot({path: 'example.png'});
-		
-			//read price and title
-			await page.waitForSelector(pruduct.selectors.tittle);
-			let tittle = await page.$eval(pruduct.selectors.tittle, (el) => el.innerText);
-			tittle = tittle.trim();
-		
-			await page.waitForSelector(pruduct.selectors.price);
-			let price = await page.$eval(pruduct.selectors.price, (el) => el.innerText);
-			price = price.trim().replace(/[^0-9]/g, "");
+			// await page.goto(pruduct.url);
+			// await page.screenshot({path: pruduct.selectors.pic + 'example.png'});
+			
+			let tittle, price;
+			let webpage = "";
 
-			const result = `${date} - ${tittle} - ${price} ${price < el.targetPrice ? targetPriceStr : ""}`;
+			try {
+				await page.goto(pruduct.url);
+			} catch (error) {
+				webpage = "hibás elérési útvonal";
+			}
 
+			// let tittle;
+			// await new Promise((resolve, reject) => {
+			// 	if(page.waitForSelector(pruduct.selectors.tittle)){
+			// 		resolve(tittle = page.$eval(pruduct.selectors.tittle, (el) => el.innerText));
+			// 	}else{
+			// 		reject(tittle = "");
+			// 	}
+			// 	tittle = tittle.trim();
+			// });
+		
+			// let price;
+			// await new Promise((resolve, reject) => {
+			// 	if(page.waitForSelector(pruduct.selectors.price)){
+			// 		resolve(price = page.$eval(pruduct.selectors.price, (el) => el.innerText));
+			// 	}else{
+			// 		reject(price = "");
+			// 	}
+			// 	price = price.trim().replace(/[^0-9]/g, "");
+			// });
+
+			try {
+				await page.waitForSelector(pruduct.selectors.tittle);
+				tittle = await page.$eval(pruduct.selectors.tittle, (el) => el.innerText);
+				tittle = tittle.trim();
+			} catch (error) {
+				tittle = "sikertelen lekérdezés";
+			}
+			try {
+				await page.waitForSelector(pruduct.selectors.price);
+				price = await page.$eval(pruduct.selectors.price, (el) => el.innerText);
+				price = price.trim().replace(/[^0-9]/g, "");
+			} catch (error) {
+				price = "sikertelen lekérdezés";
+			}
+
+			// read price and title
+			// await page.waitForSelector(pruduct.selectors.tittle);
+			// let tittle = await page.$eval(pruduct.selectors.tittle, (el) => el.innerText);
+			// tittle = tittle.trim();
+			
+			// await page.waitForSelector(pruduct.selectors.price);
+			// let price = await page.$eval(pruduct.selectors.price, (el) => el.innerText);
+			// price = price.trim().replace(/[^0-9]/g, "");
+			
+			const result = `${date} - ${webpage} ${tittle} - ${price} ${price < el.targetPrice ? targetPriceStr : ""}`;
+			
 			logger.write(result + "\n");
+
+			count++;
+			console.log(count);
 		}
 	}
 
