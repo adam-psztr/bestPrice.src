de1a279f5ce0d6a469d3fd1e6467590429e92dad
Pasztor Adam <adam.psztr@icloud.com>
2022-10-26 15:08:28 +0200

    special price error correction
    special price error correction, sorting bugfix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // module.exports = [
 // 	{
-// 		product: "product",
+// 		product: "productMin",
 // 		mainProductName: "mainProductName",
 // 		subProductName: "subProductName"
 // 		sellers: [
@@ -10,11 +10,19 @@
 // 				selectors: {
 // 					title: "productTitleSelector",
 // 					price: "productPriceSelector"
+// 					discountPrice: "productDiscountPriceSelector"
 // 				}
 // 			}
 // 		]
 // 	}
 // ]
+
+// product selector template
+let pst = {
+	spar: {
+		title: "#product-361462004"
+	}
+}
 
 module.exports = [
 	{
@@ -27,7 +35,8 @@ module.exports = [
 				url: "https://www.apple.com/hu/shop/buy-iphone/iphone-14-pro",
 				selectors: {
 					title: "span > span.form-selector-left-col.column.large-6 > span.form-selector-title",
-					price: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span"
+					price: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span",
+					discountPrice: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span"
 				}
 			},
 			{
@@ -35,7 +44,8 @@ module.exports = [
 				url: "https://istyle.hu/iphone-14-pro.html?config_size=3574&color=3592&config_model=1322",
 				selectors: {
 					title: "#col-info-main > div > div.page-title-wrapper.product > h1 > span",
-					price: "#product-options-wrapper > div > div > div.swatch-attribute.config_model.show_discount_badge.show_price > div > div.swatch-option.selected > span.price"
+					price: "#product-options-wrapper > div > div > div.swatch-attribute.config_model.show_discount_badge.show_price > div > div.swatch-option.selected > span.price",
+					discountPrice: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span"
 				}
 			},
 			{
@@ -43,7 +53,8 @@ module.exports = [
 				url: "https://www.mediamarkt.hu/hu/product/_apple-iphone-14-pro-128-gb-asztrofekete-kártyafüggetlen-okostelefon-1396917.html",
 				selectors: {
 					title: "#product-details > div.details > h1",
-					price: "#product-details > div.details > div.product-attributes.js-product-attributes > div.product-attributes__group.product-attributes__group--colors > div.product-attributes__color-item.product-attributes__color-item--active > span"
+					price: "#product-details > div.details > div.product-attributes.js-product-attributes > div.product-attributes__group.product-attributes__group--colors > div.product-attributes__color-item.product-attributes__color-item--active > span",
+					discountPrice: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span"
 				}
 			}
 		]
@@ -58,7 +69,8 @@ module.exports = [
 				url: "http://books.toscrape.com/catalogue/the-first-hostage-jb-collins-2_749/index.html",
 				selectors: {
 					title: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
-					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
+					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color",
+					discountPrice: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span"
 				}
 			},
 			{
@@ -66,7 +78,8 @@ module.exports = [
 				url: "http://books.toscrape.com/catalogue/thirst_946/index.html",
 				selectors: {
 					title: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
-					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
+					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color",
+					discountPrice: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span"
 				}
 			},
 			{
@@ -74,7 +87,42 @@ module.exports = [
 				url: "http://books.toscrape.com/catalogue/tuesday-nights-in-1980_870/index.html",
 				selectors: {
 					title: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1",
-					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color"
+					price: "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > p.price_color",
+					discountPrice: "#root > div.rf-bfe > div.rf-bfe-header-wrapper > div > div.rf-bfe-header-price-wrapper > div > div > div.rc-price > div > span > span > span"
+				}
+			}
+		]
+	},
+	{
+		product: "tejfol",
+		mainProductName: "Mizo tejföl 20%",
+		subProductName: "330 g",
+		sellers: [
+			{
+				seller: "Tesco",
+				url: "https://bevasarlas.tesco.hu/groceries/hu-HU/products/2004003218682",
+				selectors: {
+					title: "#main > div.main__content > div > div:nth-child(1) > div.product-details-page > div.product-overview > div:nth-child(1) > div > div.product-details-tile__main > div.product-details-tile__title-wrapper > h1",
+					price: "#main > div.main__content > div > div:nth-child(1) > div.product-details-page > div.product-overview > div:nth-child(1) > div > div > div.product-controls__wrapper > form > div > div.price-details--wrapper > div.price-control-wrapper > div > div > span > span.value",
+					discountPrice: "#main > div.main__content > div > div:nth-child(1) > div.product-details-page > div.product-overview > div:nth-child(1) > div > div.product-details-tile__main > div:nth-child(3) > ul > li > a > div > span.offer-text"
+				}
+			},
+			{
+				seller: "SPAR",
+				url: "https://www.spar.hu/onlineshop/mizo-tejfol-20-330-g/p/361462004",
+				selectors: {
+					title: pst.spar.title,
+					price: "#productDetailsPageCenterWrapper > div.productDetailsPanel > div.productMainDetails.cf > div.productMainDetailsPrice.productQtyFav.productInformationContainer > div.productMainDetailsPriceLabels > label.productDetailsPrice",
+					discountPrice: "#productDetailsPageCenterWrapper > div.productDetailsPanel > div.productMainDetails.cf > div.productMainDetailsPrice.productQtyFav.productInformationContainer > div.productMainDetailsPriceLabels > label.productDetailsOldPrice"
+				}
+			},
+			{
+				seller: "kifli",
+				url: "https://www.kifli.hu/32167-mizo-tejfol-20",
+				selectors: {
+					title: "#productDetail > div.sc-c64788e3-1.bShmSQ > div.sc-c64788e3-5.jDPiWs > h2 > a",
+					price: "#productDetail > div.sc-c64788e3-1.bShmSQ > div.sc-c64788e3-5.jDPiWs > div:nth-child(4) > div.sc-93217d65-0.dUvcGB > div",
+					discountPrice: "#productDetail > div.sc-c64788e3-1.bShmSQ > div.sc-c64788e3-5.jDPiWs > div:nth-child(4) > div.sc-93217d65-0.dUvcGB > div"
 				}
 			}
 		]
--- index.js
+++ index.js
@@ -15,17 +15,19 @@ const config = require("./config");
 	}});
 	
 	//open browser
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({headless: true, executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'});
 	const context = await browser.createIncognitoBrowserContext();
   const page = await context.newPage();
 	await page.setViewport({width: 1920, height: 1080});
-	await page.setUserAgent(randomUserAgent.getRandom());
+	await page.setUserAgent(randomUserAgent.getRandom(function (ua) {
+    return parseFloat(ua.browserVersion) >= 80;
+	}));
 	
 	let productCount = 1;
 	let sellerCount = 1;
 	let queryContent = {};
 	let productPriceArray = [];
-	
+
 	for (const el of config) {
 
 		console.log(`product(${productCount}/${config.length}): ${el.mainProductName} ${el.subProductName} -----v`);
@@ -43,7 +45,7 @@ const config = require("./config");
 		
 		for (const seller of el.sellers) {
 			
-			let title, price;
+			let title, price, discountPrice;
 			let sellerwebpage = "";
 			let errorMessage = "";
 			
@@ -66,33 +68,63 @@ const config = require("./config");
 			try {
 				await page.waitForSelector(seller.selectors.price);
 				price = await page.$eval(seller.selectors.price, (el) => el.innerText);
-				price = price.trim().replace(/[^0-9]/g, "");
+				switch(seller.seller) {
+					case "kifli": price = price.match(/\d+/g)[0]; break;
+					default: price =  price.trim().replace(/[^0-9]/g, "");
+				}
+					// price = price.match(/\d+/g)[0];
+					// price = price.trim().replace(/[^0-9]/g, "");				
 			} catch (error) {
 				errorMessage == "" ? errorMessage = "wrong price selector" : errorMessage += " * wrong price selector";
-				price = "0";
+				price = "999999999";
+			};
+			
+			try {
+				await page.waitForSelector(seller.selectors.discountPrice);
+				discountPrice = await page.$eval(seller.selectors.discountPrice, (el) => el.innerText);
+				switch(seller.seller) {
+					case "kifli": discountPrice = discountPrice.match(/\d+/g)[0]; break;
+					default: discountPrice =  discountPrice.trim().replace(/[^0-9]/g, "");
+				}
+				if (discountPrice == "") {
+					discountPrice = "999999999";
+					errorMessage == "" ? errorMessage = "OK" : errorMessage += " * no discount price";
+				} 
+			} catch (error) {
+				errorMessage == "" ? errorMessage = "OK" : errorMessage += " * no discount price";
+				discountPrice = "999999999";
 			};
 			
 			queryContent[el.product]["productSellers"].push({
 				productSeller: sellerwebpage == "" ? seller.seller : sellerwebpage,
 				productName: title,
 				productPrice: price,
+				productDiscountPrice: discountPrice,
 				productLink: seller.url
 			});
 
+			if(discountPrice != "999999999") {
+				price = discountPrice;
+			}
+
 			sellerPriceArray.push(price);
 			
-			console.log(errorMessage == "" ? ` > seller(${sellerCount}/${el.sellers.length}): ${seller.seller} >> OK` : `!> seller(${sellerCount}/${el.sellers.length}): ${seller.seller} >> ` + errorMessage);
+			console.log(errorMessage == "OK" ? ` > seller(${sellerCount}/${el.sellers.length}): ${seller.seller} >> ` + errorMessage : errorMessage == ""? ` > seller(${sellerCount}/${el.sellers.length}): ${seller.seller} >> OK+` : `!> seller(${sellerCount}/${el.sellers.length}): ${seller.seller} >> ` + errorMessage);
 			sellerCount++;
 		}
 
-		sellerPriceArray.sort();
+		sellerPriceArray.sort((a, b) => a - b);
 
 		let productSellersArray = queryContent[el.product]["productSellers"];
 		let temporaryProductSellersArray = [];
 
 		while (productSellersArray.length>0) {
 			let firstSeEl = productSellersArray.shift();
-			temporaryProductSellersArray[sellerPriceArray.indexOf(firstSeEl.productPrice)] = firstSeEl;
+			if(firstSeEl.productDiscountPrice == "999999999"){
+				temporaryProductSellersArray[sellerPriceArray.indexOf(firstSeEl.productPrice)] = firstSeEl;
+			}else {
+				temporaryProductSellersArray[sellerPriceArray.indexOf(firstSeEl.productDiscountPrice)] = firstSeEl;
+			}
 		};
 
 		queryContent[el.product]["productSellers"] = temporaryProductSellersArray;
@@ -103,7 +135,7 @@ const config = require("./config");
 		productCount++;
 	}
 
-	productPriceArray.sort();
+	productPriceArray.sort((a, b) => a - b);
 
 	let temporaryProductsArray = [];
 
@@ -111,7 +143,11 @@ const config = require("./config");
 
 	while (queryContentKeys.length>0) {
 		let firstPrEl = queryContentKeys.shift();
-		temporaryProductsArray[productPriceArray.indexOf(queryContent[firstPrEl]["productSellers"][0].productPrice)] = firstPrEl;
+		if(queryContent[firstPrEl]["productSellers"][0].productDiscountPrice == "999999999"){
+			temporaryProductsArray[productPriceArray.indexOf(queryContent[firstPrEl]["productSellers"][0].productPrice)] = firstPrEl;
+		} else {
+			temporaryProductsArray[productPriceArray.indexOf(queryContent[firstPrEl]["productSellers"][0].productDiscountPrice)] = firstPrEl;
+		}
 	};
 
 	queryContent.sortArray = {
